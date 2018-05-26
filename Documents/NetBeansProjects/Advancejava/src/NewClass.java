558838192d1e0979593386be2de91c832d38affc
Prachi <ditprachi@gmail.com>
2018-05-26 12:26:17 +0530

    First commit after creating a sample to enter students and their details using add and display button.

---
+++ Documents/NetBeansProjects/Advancejava/src/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class NewClass {
+    String studentName;
+    String course;
+    int fees;
+void add()    
+    
+}
